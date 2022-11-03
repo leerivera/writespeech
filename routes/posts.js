b89ac0da15971f962988c7265231d121b4bf5c8e
leerivera <leerivera80@gmail.com>
2022-11-03 12:05:48 -0400

    Revert "included partials"
    This reverts commit 99dfa7acdc403033df6e8692ea22bfff239a1f81.

--- routes/posts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const postsController = require("../controllers/posts")
-
-//Post Routes
-router.post("/createPost", postsController.createPost)
-
-module.exports = router
